f313d948420601d5135465f551df91823a858fa5
Hana Kluch Duchková <hana.duchkova@icloud.com>
2022-03-24 20:22:45 +0100

    + api

--- src/Pages/Vyhledavac/index.jsx
+++ src/Pages/Vyhledavac/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useQuery } from "react-query";
 import "./vyhledavac.css";
 
+const getMedicaments = async () => {
+  const response = await fetch("/api/medicaments");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
 const Vyhledavac = () => {
+  const query = useQuery("medicaments", getMedicaments);
+
   return (
     <div className="vyhledavac-container">
       <p>vyhledávač</p>
