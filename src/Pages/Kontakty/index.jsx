09a467a5bb96fc566ae487ad35dc96ef1ddf30b6
Hana Kluch Duchková <hana.duchkova@icloud.com>
2022-04-15 18:41:46 +0200

    kontakty fetch, zobrazení, filtr, design

--- src/Pages/Kontakty/index.jsx
+++ src/Pages/Kontakty/index.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import "./kontakty.css";
 
 const myApi = "DmzCgfsbqUwKBwUfe57y1v24tXkXDG81htWBc5qh";
 
 const getMedicalFacilities = async () => {
-  const response = await fetch(`https://api.apitalks.store/zdravotni-sluzby`, {
-    headers: { "x-api-key": myApi },
+  const query = new URLSearchParams({
+    filter: `{"where":{"OborPece":"Adiktolog"}}`,
   });
+  const response = await fetch(
+    `https://api.apitalks.store/zdravotni-sluzby?${query}`,
+    {
+      headers: { "x-api-key": myApi },
+    }
+  );
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
@@ -15,60 +21,157 @@ const getMedicalFacilities = async () => {
 };
 
 const Kontakty = () => {
+  const [kraj, setKraj] = useState("");
   const { data, isLoading } = useQuery(
     "medicalFacilities",
     getMedicalFacilities
   );
-  console.log(data);
+
   return (
     <div className="kontakty-container">
-      <p>kontakty</p>
+      <h2 className="kontakty-container__header">Adiktologická centra v ČR</h2>
       <div className="kontakty-kraje">
-        <p className="kontakty-kraje__p p--praha">Hlavní město Praha</p>
-        <p className="kontakty-kraje__p p--stredocesky">Středočeský kraj</p>
-        <p className="kontakty-kraje__p p--jihocesky">Jihočeský kraj</p>
-        <p className="kontakty-kraje__p p--plzensky">Plzeňský kraj</p>
-        <p className="kontakty-kraje__p p--karlovarsky">Karlovarský kraj</p>
-        <p className="kontakty-kraje__p p--ustecky">Ústecký kraj</p>
-        <p className="kontakty-kraje__p p--liberecky">Liberecký kraj</p>
-        <p className="kontakty-kraje__p p--kralovehradecky">
+        <p
+          className="kontakty-kraje__p p--vse"
+          id=""
+          onClick={() => setKraj("")}
+        >
+          Vše
+        </p>
+        <p
+          className="kontakty-kraje__p p--praha"
+          id="CZ010"
+          onClick={() => setKraj("CZ010")}
+        >
+          Hlavní město Praha
+        </p>
+        <p
+          className="kontakty-kraje__p p--stredocesky"
+          id="CZ020"
+          onClick={() => setKraj("CZ020")}
+        >
+          Středočeský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--jihocesky"
+          id="CZ031"
+          onClick={() => setKraj("CZ031")}
+        >
+          Jihočeský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--plzensky"
+          id="CZ032"
+          onClick={() => setKraj("CZ032")}
+        >
+          Plzeňský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--karlovarsky"
+          id="CZ041"
+          onClick={() => setKraj("CZ041")}
+        >
+          Karlovarský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--ustecky"
+          id="CZ042"
+          onClick={() => setKraj("CZ042")}
+        >
+          Ústecký kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--liberecky"
+          id="CZ051"
+          onClick={() => setKraj("CZ051")}
+        >
+          Liberecký kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--kralovehradecky"
+          id="CZ052"
+          onClick={() => setKraj("CZ052")}
+        >
           Královéhradecký kraj
         </p>
-        <p className="kontakty-kraje__p p--pardubicky">Pardubický kraj</p>
-        <p className="kontakty-kraje__p p--vysocina">Kraj Vysočina</p>
-        <p className="kontakty-kraje__p p--jihomoravsky">Jihomoravský kraj</p>
-        <p className="kontakty-kraje__p p--zlinsky">Zlínský kraj</p>
-        <p className="kontakty-kraje__p p--olomoucky">Olomoucký kraj</p>
-        <p className="kontakty-kraje__p p--moravskoslezsky">
+        <p
+          className="kontakty-kraje__p p--pardubicky"
+          id="CZ053"
+          onClick={() => setKraj("CZ053")}
+        >
+          Pardubický kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--vysocina"
+          id="CZ063"
+          onClick={() => setKraj("CZ063")}
+        >
+          Kraj Vysočina
+        </p>
+        <p
+          className="kontakty-kraje__p p--jihomoravsky"
+          id="CZ064"
+          onClick={() => setKraj("CZ064")}
+        >
+          Jihomoravský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--zlinsky"
+          id="CZ072"
+          onClick={() => setKraj("CZ072")}
+        >
+          Zlínský kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--olomoucky"
+          id="CZ071"
+          onClick={() => setKraj("CZ071")}
+        >
+          Olomoucký kraj
+        </p>
+        <p
+          className="kontakty-kraje__p p--moravskoslezsky"
+          id="CZ080"
+          onClick={() => setKraj("CZ080")}
+        >
           Moravskoslezský kraj
         </p>
       </div>
       <div className="kontakty-results">
         {isLoading && <p>Loading...</p>}
         {data &&
-          data.data.map((medFac) => (
-            <div className="kontakty-results__card">
-              <h3 className="kontakty-results__card__h3">
-                {medFac.NazevZarizeni}
-              </h3>
-              <p className="kontakty-results__card__p druhZarizeni">
-                {medFac.DruhZarizeni}
-              </p>
-              <p className="kontakty-results__card__p obec">{medFac.Obec}</p>
-              <p className="kontakty-results__card__p adresa">
-                {medFac.Ulice} {medFac.CisloDomovniOrientacni}
-              </p>
-              <p className="kontakty-results__card__p telefon">
-                tel: {medFac.PoskytovatelTelefon}
-              </p>
-              <p className="kontakty-results__card__p email">
-                email: {medFac.PoskytovatelEmail}
-              </p>
-              <p className="kontakty-results__card__p web">
-                web: {medFac.PoskytovatelWeb}
-              </p>
-            </div>
-          ))}
+          data.data
+            .filter((el) => el.KrajCode.includes(kraj))
+            .map((medFac) => (
+              <div className="kontakty-results__card">
+                <h3 className="kontakty-results__card__h3">
+                  {medFac.NazevZarizeni}
+                </h3>
+                <p className="kontakty-results__card__p druhZarizeni">
+                  {medFac.DruhZarizeni}
+                </p>
+
+                <p className="kontakty-results__card__p adresa">
+                  {medFac.Ulice} {medFac.CisloDomovniOrientacni}, {medFac.Obec}
+                </p>
+                <p className="kontakty-results__card__p telefon">
+                  tel: {medFac.PoskytovatelTelefon}
+                </p>
+                <p className="kontakty-results__card__p email">
+                  email: {medFac.PoskytovatelEmail}
+                </p>
+                <p className="kontakty-results__card__p web">
+                  web:{" "}
+                  <a
+                    href={medFac.PoskytovatelWeb}
+                    target="_blank"
+                    className="kontakty-results__card__a web--a"
+                  >
+                    {medFac.PoskytovatelWeb}
+                  </a>
+                </p>
+              </div>
+            ))}
       </div>
     </div>
   );
