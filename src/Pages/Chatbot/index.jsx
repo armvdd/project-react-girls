f3f4ba4c461fd801936a373033b4b31360e9fce6
Hana Kluch Duchková <hana.duchkova@icloud.com>
2022-05-07 11:58:47 +0200

    chatbot otázky, odpovědi, counter, zobrazení výsledku

--- src/Pages/Chatbot/index.jsx
+++ src/Pages/Chatbot/index.jsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import "./chatbot.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { render } from "@testing-library/react";
 
 const getChatbot = async () => {
   const response = await fetch(`/api/chatbot`);
@@ -20,6 +21,11 @@ const ChatbotPage = () => {
   const [bubbleVisible, setBubbleVisible] = useState(false);
   const [userQuestions, setUserQuestions] = useState(false);
   const [otherQuestions, setOtherQuestions] = useState(false);
+
+  const [id, setId] = useState(1);
+  const [answered, setAnswered] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [results, setResults] = useState(false);
 
   useEffect(() => {
     fetch(`/api/chatbot`)
@@ -46,6 +52,16 @@ const ChatbotPage = () => {
     setOtherQuestions(true);
     setUserQuestions(false);
   };
+  const renderResults = () => {
+    if (counter < 4) {
+      return <p>Celkem na cajk</p>;
+    } else if (counter > 4 && counter < 7) {
+      return <p>Se to blíží k průseru</p>;
+    } else if (counter >= 7) {
+      return <p>Kámo, dělej s tím něco!</p>;
+    }
+  };
+
   return (
     <div className="chatbot-cotainer">
       {/* HTML ver questionBox
@@ -82,19 +98,54 @@ const ChatbotPage = () => {
         </div>
         <div>
           {data && userQuestions
-            ? data.questions.map((question) => (
-                <>
-                  <div className="bubble">
-                    <p className="bubble__question">{question.question}</p>
-                  </div>
-                  <div className="bubble2">
-                    {data &&
-                      question.answers.map((answer) => (
-                        <p className="bubble__answer">{answer.title}</p>
-                      ))}
-                  </div>
-                </>
-              ))
+            ? data.questions
+                .filter((question) => question.id === id)
+                .map((question) => (
+                  <>
+                    <div className="bubble">
+                      <p className="bubble__question">{question.title}</p>
+                    </div>
+                    <div className="bubble pulse" key={question.question}>
+                      <p className="bubble__question">
+                        otázka č. {question.id}/5
+                      </p>
+                      <p className="bubble__question">{question.question}</p>
+                    </div>
+                    <div className="bubble2">
+                      {data &&
+                        question.answers.map((answer) => (
+                          <p
+                            className="bubble__answer bubble__answer--test"
+                            onClick={() => {
+                              if (question.id < 5) {
+                                setId(question.id + 1);
+                                setCounter(counter + answer.points);
+                              } else if (question.id === 5) {
+                                setCounter(counter + answer.points);
+                                setResults(true);
+                              }
+                              setAnswered(answered + 1);
+                              if (answered >= 5) {
+                                setCounter(counter + 0);
+                              }
+                              console.log(results);
+                            }}
+                          >
+                            {answer.title}
+                          </p>
+                        ))}
+                    </div>
+                    <div className="bubble">
+                      <p className="bubble__question">points: {counter}</p>
+                      <p className="bubble__question">answered: {answered}</p>
+                    </div>
+                    {results ? (
+                      <div className="bubble pulse">
+                        <p>{renderResults()}</p>
+                      </div>
+                    ) : null}
+                  </>
+                ))
             : null}
         </div>
 
